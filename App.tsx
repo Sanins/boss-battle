cc11f41f1a92a18cbaf9376ddf3020685a45bc40
Robert Sanins <rob.sanins@lovehoney.co.uk>
2020-02-03 15:13:54 +0000

    move navigation out of App.tsx

--- App.tsx
+++ App.tsx
@@ -1,27 +1,8 @@
-import {createAppContainer} from 'react-navigation';
-import {createStackNavigator} from 'react-navigation-stack';
-import LoginScreen from './screens/LoginScreen';
-import BattleScreen from './screens/BattleScreen';
+import React from 'react'
+import AppNavigator from './components/Navigation/Navigation'
 
-const MainNavigator = createStackNavigator({
-  Home: {
-    screen: LoginScreen,
-    navigationOptions: ({ navigation }) => {
-      return {
-        headerShown: false
-      }
-    }
-  },
-  Profile: {
-    screen: BattleScreen,
-    navigationOptions: ({ navigation }) => {
-      return {
-        headerShown: false
-      }
-    }
-  },
-});
-
-const App = createAppContainer(MainNavigator);
-
-export default App;
+export default function App() {
+  return (
+    <AppNavigator />
+  )
+}
---
+++ components/Navigation/Navigation.tsx
@@ -0,0 +1,25 @@
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+import LoginScreen from './../../screens/LoginScreen';
+import BattleScreen from './../../screens/BattleScreen';
+
+const MainNavigator = createStackNavigator({
+  Home: {
+    screen: LoginScreen,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerShown: false
+      }
+    }
+  },
+  Profile: {
+    screen: BattleScreen,
+    navigationOptions: ({ navigation }) => {
+      return {
+        headerShown: false
+      }
+    }
+  },
+});
+
+export default createAppContainer(MainNavigator);
